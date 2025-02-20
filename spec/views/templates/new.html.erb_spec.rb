f43fdf668934e9fc685d519100442bf9916d9dd4
GuilhermeBDias <guilhermebdias55@gmail.com>
2025-02-19 21:48:31 -0300

    ✅:test
    adicionei mais alguns testes a view new do template

--- spec/views/templates/new.html.erb_spec.rb
+++ spec/views/templates/new.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "templates/new.html.erb", type: :view do
-  before do
+  before(:each) do
     assign(:template, Template.new)
     render
   end
@@ -33,5 +33,41 @@ RSpec.describe "templates/new.html.erb", type: :view do
 
   it "tem um botão de submit" do
     expect(rendered).to have_button("Criar")
+  end
+
+  describe "Error messages" do
+    before do
+      template = Template.new
+      template.valid?
+      assign(:template, template)
+      render
+    end
+
+    it "displays error messages when template is invalid" do
+      expect(rendered).to have_selector("#error_explanation")
+    end
+  end
+
+  describe "Dynamic question fields" do
+    before do
+      assign(:template, Template.new)
+      render
+    end
+
+    it "displays the add question button" do
+      expect(rendered).to have_button("Adicionar questão")
+    end
+
+    it "hidden template contains fields for new question" do
+      within '#questions' do
+        expect(rendered).to have_selector("template#question-template .question-fields")
+      end
+    end
+
+    it "displays a remove question button within the question fields" do
+      within '#questions' do
+        expect(rendered).to have_selector("button.remove-question")
+      end
+    end
   end
 end
