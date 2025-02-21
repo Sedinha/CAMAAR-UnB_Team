5e65fc58c1bc1e8745fe216da154bab7b5c4635d
GuilhermeBDias <guilhermebdias55@gmail.com>
2025-02-21 12:57:08 -0300

    🐛:fix
    algumas pequenas alterações para funcionar.

--- app/models/questao_option.rb
+++ app/models/questao_option.rb
@@ -1,5 +1,5 @@
 class QuestaoOption < ApplicationRecord
   belongs_to :question
 
-  validates :nome, :texto, :questao_id, presence: true
+  validates :nome, :texto, :question_id, presence: true #alteração feita para passar no teste estava questao.id e não question_id
 end
